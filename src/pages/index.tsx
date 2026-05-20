215a115831f7213fb2199cc2a3151e98d6441d70
Ben Edwards <ben.edwards@easyjet.com>
2026-05-20 12:48:41 +0100

    feat: messages (#10)
    * feat: messages
    
    * chore: style tweak

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
+import { VisuallyHidden } from '@newhighsco/chipset'
 import type { NextPage } from 'next'
 import { LogoJsonLd, SocialProfileJsonLd } from 'next-seo'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import urlJoin from 'url-join'
 
 import PageContainer from '~components/PageContainer'
@@ -8,20 +9,61 @@ import config from '~config'
 
 const { name, title, logo, socialLinks, url } = config
 const meta = { canonical: urlJoin(url, '/'), customTitle: true, title }
-
-const HomePage: NextPage = () => (
-  <PageContainer meta={meta}>
-    <SocialProfileJsonLd
-      type="Organization"
-      name={name}
-      url={url}
-      sameAs={Object.values(socialLinks)}
-    />
-    {logo?.bitmap && <LogoJsonLd url={url} logo={urlJoin(url, logo.bitmap)} />}
+const messages = [
+  <>
     Putting
-    <br /> the <em>you</em>
-    <br /> in <em>umavi</em>
-  </PageContainer>
-)
+    <br /> the you
+    <br /> in <em>Umavi</em>.
+  </>,
+  <>
+    <em>
+      <del>Typhros</del>
+    </em>
+    <br />
+    No fuss<em>.</em>
+  </>,
+  <>
+    <em>Dunamis</em>
+    <br />
+    Out<em>.</em>
+  </>,
+  <>
+    <span aria-hidden>
+      B<small>∞</small>k
+    </span>
+    <VisuallyHidden>Book</VisuallyHidden>
+    <br />
+    your
+    <br />
+    <em>consecution</em>
+    <br />
+    <strong>today</strong>
+  </>
+]
+
+const HomePage: NextPage = () => {
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const index = Math.floor(Math.random() * messages.length)
+
+    setIndex(index)
+  }, [])
+
+  return (
+    <PageContainer meta={meta}>
+      <SocialProfileJsonLd
+        type="Organization"
+        name={name}
+        url={url}
+        sameAs={Object.values(socialLinks)}
+      />
+      {logo?.bitmap && (
+        <LogoJsonLd url={url} logo={urlJoin(url, logo.bitmap)} />
+      )}
+      {messages[index]}
+    </PageContainer>
+  )
+}
 
 export default HomePage
