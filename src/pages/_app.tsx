d2e95ab1e6f21c8d6aebad6e4d7d22ed07516306
Ben Edwards <hello@benedfit.com>
2026-07-10 15:54:27 +0100

    feat(fonts): use mixins and functions to preload

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,22 +5,11 @@ import { type AppProps } from 'next/app'
 import React, { type FC } from 'react'
 
 import config from '~config'
+import { default as fonts } from '~styles/font-faces.module.scss'
 import theme from '~theme'
 
-const fonts = { inter: { extension: 'woff2' } }
-
-const meta = {
-  additionalLinkTags: Object.entries(fonts).map(([font, { extension }]) => ({
-    rel: 'preload',
-    href: `/fonts/${font}.${extension}`,
-    as: 'font',
-    type: `font/${extension}`,
-    crossOrigin: 'anonymous'
-  }))
-}
-
 const App: FC<AppProps> = props => (
-  <AppPage {...props} theme={theme} config={config} meta={meta} />
+  <AppPage {...props} theme={theme} config={config} fonts={fonts} />
 )
 
 export default App
